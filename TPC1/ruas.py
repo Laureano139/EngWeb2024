318cda20c64b453ea3676b3a7adce27135bcd9c9
Hendrix <afamorim16@sapo.pt>
2024-02-18 23:49:33 +0000

    Tudo corrigido

--- TPC1/ruas.py
+++ TPC1/ruas.py
@@ -16,6 +16,7 @@ html = """
     </div>
 </head>
 <body>
+    <ul class='w3-ul w3-border w3-hoverable'>
 """
 
 template = """
@@ -36,29 +37,20 @@ ruas_dir = os.path.join(curr, 'MapaRuas-materialBase/texto')
 img_atuais = os.path.join(curr, 'MapaRuas-materialBase/atual')
 
 lista_ruas = []
+    
 
 for ficheiro in os.listdir(ruas_dir):
-    
+        
     filepath = os.path.join(ruas_dir, ficheiro)
     tree = ET.parse(filepath)
     root = tree.getroot()
 
     meta = root.find('./meta')
-    nome = meta.find('./nome')
-    numero = meta.find('./número')
-    
-    lista_ruas.append((nome.text, numero.text))
-    
-lista_ordenada = sorted(lista_ruas, key=lambda x: x[0].strip())
+    nome = meta.find('./nome').text.strip()
+    numero = meta.find('./número').text
 
-for nome, numero in lista_ordenada:
-    ruaHTML = open(f'html/{nome}.html', 'w', encoding="utf-8")
-    html += "<ul class='w3-ul w3-border w3-hoverable'>"
-    html += f'<li><a href="html/{nome}.html">{nome}</a></li>'
-    html += "</ul>"
-    with open('mapa.html', 'w', encoding="utf-8") as f:
-        f.write(html)
-        
+    lista_ruas.append(nome)
+    
     templateCidade = template
     templateCidade += f"<h1>{nome}</h1>"
     templateCidade += f"<h3>Número: {numero}</h3>"
@@ -85,30 +77,24 @@ for nome, numero in lista_ordenada:
     for fig in root.findall('./corpo/figura'):
         imagem_path = fig.find('./imagem').attrib.get('path')
         legenda = fig.find('./legenda').text
-        imagens.append((imagem_path, legenda))
-        
-    for imagem_path, legenda in imagens:
-        numero_no_arquivo = re.search(r'\d+', imagem_path).group()
-        numero_no_arquivo = int(numero_no_arquivo.lstrip('0'))
-        if(numero_no_arquivo == int(numero)):
-            partes = imagem_path.split("../imagem/")
-            imgFile = partes[1]
-            templateCidade += f"""
-                <div class="w3-third w3-card">
-                    <img src='../MapaRuas-materialBase/imagem/{imgFile}' class="w3-hover-opacity" style="width:100%">
-                    <div class="w3-container">
-                        <h5>{legenda}</h5>
-                    </div>
+        partes = imagem_path.split("../imagem/")
+        imgFile = partes[1]
+        templateCidade += f"""
+            <div class="w3-third w3-card">
+                <img src='../MapaRuas-materialBase/imagem/{imgFile}' class="w3-hover-opacity" style="width:100%">
+                <div class="w3-container">
+                    <h5>{legenda}</h5>
                 </div>
-            """
+            </div>
+        """
         
     for para in root.findall('./corpo/para'):
-            templateCidade += f'<p>{para.text}'
-                
-            for elem in para:
-                if elem.tag == 'lugar' or elem.tag == 'data' or elem.tag == 'entidade':
-                   templateCidade += f'<b>{elem.text}</b>{elem.tail}'
-            templateCidade += '</p>'
+        templateCidade += f'<p>{para.text}'
+            
+        for elem in para:
+            if elem.tag == 'lugar' or elem.tag == 'data' or elem.tag == 'entidade':
+                templateCidade += f'<b>{elem.text}</b>{elem.tail}'
+        templateCidade += '</p>'
         
     for casa in root.findall('./corpo/lista-casas/casa'):
         if casa.find('número') != None:
@@ -133,16 +119,22 @@ for nome, numero in lista_ordenada:
             templateCidade += '</p>'
         
     templateCidade += "</body>"
-    ruaHTML.write(templateCidade)
-    ruaHTML.close()
-    f.close()
+    with open(f'html/{nome}.html', 'w', encoding="utf-8") as ruaHTML:
+        ruaHTML.write(templateCidade)
         
 #######################################################################################################################
         
-html += """ <footer class="w3-container w3-center w3-teal">
+lista_ordenada = sorted(lista_ruas, key=lambda x: x[0])
+print(f"{lista_ruas}\n\n{lista_ordenada}")
+
+for rua in lista_ordenada:
+    html += f'<li><a href="html/{rua}.html">{rua}</a></li>'
+
+html += """     </ul>
+                <footer class="w3-container w3-center w3-teal">
                     <p>TPC1::AfonsoAmorim::A97569::PL3</p>
                 </footer>
         """
 
 with open('mapa.html', 'w', encoding="utf-8") as f:
-        f.write(html)
+    f.write(html)
