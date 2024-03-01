2207292ecea159c8470af53da7d1f9e72e8a2532
Hendrix <afamorim16@sapo.pt>
2024-03-01 15:40:46 +0000

    Update server.js

--- TPC3/server.js
+++ TPC3/server.js
@@ -46,7 +46,7 @@ http.createServer(function (req, res) {
             res.write("<h2><b> Elenco: </b></h2>")
             res.write("<ul class='w3-ul w3-hoverable'>")
             for(ator in dados.data[0]['cast']){
-                res.write("<li><a href='/ator/" + dados.data[0]['cast'][ator] + "'>" + dados.data[0]['cast'][ator] + "</a></li>")
+                res.write("<li><a href='/atores/" + dados.data[0]['cast'][ator] + "'>" + dados.data[0]['cast'][ator] + "</a></li>")
             }
             res.write("</ul>")
             res.write("<h2><b> Géneros: </b></h2>")
@@ -56,7 +56,7 @@ http.createServer(function (req, res) {
             }
             else{
                 for(genre in dados.data[0]['genres']){
-                    res.write("<li><a href='/géneros/'>" + dados.data[0]['genres'][genre] + "</a></li>")
+                    res.write("<li><a href='/generos/'>" + dados.data[0]['genres'][genre] + "</a></li>")
                 }
             }
             res.write("</ul>")
@@ -73,20 +73,23 @@ http.createServer(function (req, res) {
         })
     }
 
-    else if(q.pathname.match(/\/ator\/\w+/)){
-        let desig = q.pathname.substring(6)
-        var nameFormat = desig.replace(/%20/g, " ")
-        axios.get('http://localhost:3000/filmes?cast=' + nameFormat)
+    else if(q.pathname == '/atores'){
+        atoresAux = []
+        axios.get('http://localhost:3000/filmes')
         .then(dados => {
+            atores = dados.data
             res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
-            res.write("<head><link rel='stylesheet' type='text/css' href='/w3.css'><title>"+ nameFormat +"</title></head>")
+            res.write("<head><link rel='stylesheet' type='text/css' href='/w3.css'><title> Atores </title></head>")
             res.write("<body>")
-            res.write("<div class='w3-container w3-teal w3-center'> Página do Ator </div>")
-            res.write("<h2><b> Nome: </b></h2>" + "<h4>" + nameFormat + "</h4>")
-            res.write("<h2><b> Filmes onde participou: </b></h2>")
+            res.write("<div class='w3-container w3-teal w3-center'><h1> Atores </h1></div>")
             res.write("<ul class='w3-ul w3-hoverable'>")
-            for(f in dados.data){
-                res.write("<li><a href='/filmes/" + dados.data[f]._id.$oid + "'>" + dados.data[f]['title'] + "</a></li>")
+            for(i in atores){
+                for(n in atores[i].cast){
+                    if(!atoresAux.includes(atores[i].cast[n])){
+                        atoresAux.push(atores[i].cast[n])
+                        res.write("<li><a href='/atores/" + atores[i].cast[n] + "'>" + atores[i].cast[n] + "</a></li>")
+                    }
+                }
             }
             res.write("</ul>")
             res.write("<div class='w3-center'><a href='/filmes'><button class='w3-button w3-teal'>Voltar</button></a></div>")
@@ -95,8 +98,111 @@ http.createServer(function (req, res) {
             res.write("<h6> TPC3::EngWeb2024::A97569 </h6>")
             res.write("</footer></div>")
             res.end()
-        }).catch(erro => {
+        })
+        .catch(error => {
             res.write("Erro!")
+            res.end()
+        })
+    }
+
+    else if(q.pathname.match(/\/atores\/\w+([\'\w]+)?/)){
+        let desig = q.pathname.substring(8)
+        var nameFormat = desig.replace(/%20/g, " ")
+        axios.get('http://localhost:3000/filmes')
+        .then(dados => {
+            var filmes = dados.data.filter(filme => filme.cast.includes(nameFormat))
+            res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+            res.write("<head><link rel='stylesheet' type='text/css' href='/w3.css'><title>"+ nameFormat +"</title></head>")
+            res.write("<body>")
+            res.write("<div class='w3-container w3-teal w3-center'> Página do Ator </div>")
+            res.write("<h2><b> Nome: </b></h2>" + "<h4>" + nameFormat + "</h4>")
+            res.write("<h2><b> Filmes onde participou: </b></h2>")
+            res.write("<ul class='w3-ul w3-hoverable'>")
+            filmes.forEach(filme => {
+                res.write("<li><a href='/filmes/" + filme._id.$oid + "'>" + filme.title + "</a></li>")
+            })
+            res.write("</ul>")
+            res.write("<div class='w3-center'><a href='/filmes'><button class='w3-button w3-teal'>Voltar</button></a></div>")
+            res.write("</body>")
+            res.write("<div class='w3-padding-16'><footer class='w3-container w3-teal w3-center'>")
+            res.write("<h6> TPC3::EngWeb2024::A97569 </h6>")
+            res.write("</footer></div>")
+            res.end()
+        })
+        .catch(error => {
+            res.write("Erro!")
+            res.end()
+        })
+    }
+
+    else if(q.pathname == '/generos'){
+        generosAux = []
+        axios.get('http://localhost:3000/filmes')
+        .then(dados => {
+            generos = dados.data
+            res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+            res.write("<head><link rel='stylesheet' type='text/css' href='/w3.css'><title> Géneros </title></head>")
+            res.write("<body>")
+            res.write("<div class='w3-container w3-teal w3-center'><h1> Géneros </h1></div>")
+            res.write("<ul class='w3-ul w3-hoverable'>")
+
+            for(i in generos){
+                for(n in generos[i].genres){
+                    if(!generosAux.includes(generos[i].genres[n])){
+                        generosAux.push(generos[i].genres[n])
+                        res.write("<li><a href='/generos/" + generos[i].genres[n] + "'>" + generos[i].genres[n] + "</a></li>")
+                    }
+                }
+            }
+
+            res.write("</ul>")
+            res.write("<div class='w3-center'><a href='/filmes'><button class='w3-button w3-teal'>Voltar</button></a></div>")
+            res.write("</body>")
+            res.write("<div class='w3-padding-16'><footer class='w3-container w3-teal w3-center'>")
+            res.write("<h6> TPC3::EngWeb2024::A97569 </h6>")
+            res.write("</footer></div>")
+            res.end()
+        })
+        .catch(error => {
+            res.write("Erro!")
+            res.end()
+        })
+    }
+
+    else if(q.pathname.match(/\/generos\/(\w+)/)){
+        let gen = q.pathname.substring(9)
+        var generoFormat = gen.replace(/%20/g, " ")
+        axios.get('http://localhost:3000/filmes')
+        .then(dados => {
+            var filmesCat = dados.data.filter(filmeCat => filmeCat.genres.includes(generoFormat))
+
+            res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+
+            res.write("<head><link rel='stylesheet' type='text/css' href='/w3.css'><title>"+ generoFormat +"</title></head>")
+
+            res.write("<body>")
+            res.write("<div class='w3-container w3-teal w3-center'> Página da categoria </div>")
+
+            res.write("<h2><b> Categoria: </b></h2>" + "<h4>" + generoFormat + "</h4>")
+
+            res.write("<h2><b> Filmes da categoria: </b></h2>")
+            res.write("<ul class='w3-ul w3-hoverable'>")
+
+            filmesCat.forEach(filmeCat => {
+                res.write("<li><a href='/filmes/" + filmeCat._id.$oid + "'>" + filmeCat.title + "</a></li>")
+            })
+
+            res.write("</ul>")
+            res.write("<div class='w3-center'><a href='/filmes'><button class='w3-button w3-teal'>Voltar</button></a></div>")
+            res.write("</body>")
+            res.write("<div class='w3-padding-16'><footer class='w3-container w3-teal w3-center'>")
+            res.write("<h6> TPC3::EngWeb2024::A97569 </h6>")
+            res.write("</footer></div>")
+            res.end()
+        })
+        .catch(error => {
+            res.write("Erro!")
+            res.end()
         })
     }
     
